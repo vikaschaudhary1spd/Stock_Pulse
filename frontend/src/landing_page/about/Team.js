5420a6afa9af89d5f3c7512bf7dc11589fb76c47
Vikas Chaudhary <vikaschaudhary1spd@gmail.com>
2025-04-19 17:35:30 +0530

    update about section

--- frontend/src/landing_page/about/Team.js
+++ frontend/src/landing_page/about/Team.js
@@ -16,7 +16,7 @@ function Team() {
         </div>
         <div className="col-1"></div>
         <div className="col-4">
-          <p>Vikas Chaudhary was born on May 17,2001 in Basti, Uttar Pradesh. He is an experienced investor , Certify in SEBI registered-investor certification and holds master's degree in MCA. Stock Pulse was founded by Vikas Chaudhary in 2020 </p>
+          <p>Vikas Chaudhary was born on May 17,2001 in Basti, Uttar Pradesh. He is an experienced investor , Certify in SEBI registered- NISM -Investor certification and holds master's degree in MCA. Stock Pulse was founded by Vikas Chaudhary in 2020. </p>
         <img src='media/certi.png' style={{width:"105%" ,}}/>
         <p className="text-bold">Contact</p> 
         < a href="https://mail.google.com/mail/u/0/">Gmail</a>
@@ -24,37 +24,9 @@ function Team() {
         
         <a href="https://www.instagram.com/vikas_chaudhary170?utm_source=qr&igsh=d3ZkaGJydWdpYTM2 ">Instagram</a>
         </div>
-        {/* <div className="col-2"></div> */}
+       
         </div> 
-      {/* <div className="row p-5  ">
-        <div className="col-6 p-5">
-          <div
-            style={{
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-              height: "100vh", 
-            }}
-          >
-            <img
-              src="media/vikas2.png"
-              style={{ width: "60%", borderRadius: "5%" }}
-              alt="Vikas"
-            />
-          </div>
-
-          
-          <p className="text-center">
-            {" "}
-            Vikas Chaudhary
-            <br />
-            founder
-          </p>
-        </div>
-        <div className="col-6">
-          <p className="text-sm"></p>
-        </div> */}
-      {/* </div> */}
+    
     </div>
   );
 }
